9ad19a2c753754322d4b0edadda80208c5341dd3
Önder Koşar <36846931+onderkosar@users.noreply.github.com>
2022-06-18 16:19:29 +0300

    project organization refactoring

--- OKTMDb/Screens/Detail/DetailViewController.swift
+++ OKTMDb/Screens/Detail/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  OKTMDb
 //
 //  Created by Önder Koşar on 18.06.2022.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetailViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ OKTMDb/Screens/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  OKTMDb
+//
+//  Created by Önder Koşar on 18.06.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
